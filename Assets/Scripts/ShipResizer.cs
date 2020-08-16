219d0fcbf351e6c7689a38ee2cde7035e8e23ac4
iliya <pankihoi777@icloud.com>
2020-08-16 20:23:08 +0400

    Bouncing ships

---
+++ Assets/Scripts/ShipResizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipResizer : MonoBehaviour
+{
+ const float TotalResizeSeconds = 1;
+    float elapsedResizeSeconds = 0;
+
+    // resizing control
+    const float ScaleFactorPerSecond = 0.05f;
+    int scaleFactorSignMultiplier = 1;
+
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        // resize the game object
+        Vector3 newScale = transform.localScale;
+        
+        newScale.y += scaleFactorSignMultiplier * ScaleFactorPerSecond * Time.deltaTime;
+        
+        transform.localScale = newScale;
+
+        // update timer and check if it's done
+        elapsedResizeSeconds += Time.deltaTime;
+        if (elapsedResizeSeconds >= TotalResizeSeconds) {
+
+            // reset timer and start resizing the game object
+            // in the opposite direction
+            elapsedResizeSeconds = 0;
+            scaleFactorSignMultiplier *= -1;
+        }
+	}
+}

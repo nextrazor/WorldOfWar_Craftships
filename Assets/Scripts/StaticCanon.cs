83fbae5d83de928ad08273f9ab19b09549057602
nexty777 <pankihoi777@icloud.com>
2020-08-29 17:37:37 +0300

    123
    123

---
+++ Assets/Scripts/StaticCanon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCanon : MonoBehaviour
+{
+    public float elapsedtime = 0;
+    public float totaltime = 1;
+    public GameObject cannonball;
+    public GameObject canon;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (elapsedtime > 2)
+        {
+            GameObject firedBall = Instantiate(cannonball, canon.transform.position, canon.transform.rotation);
+            elapsedtime = 0;
+        }
+        else
+        {
+            elapsedtime += Time.deltaTime;
+        }
+    }
+}
